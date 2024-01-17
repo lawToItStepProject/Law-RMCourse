47b537e08cc1f31de8849b762be3589167277e20
Irina Makedonska <iriwamaked@gmail.com>
2024-01-17 20:42:55 +0200

    Добавлена базовая структура

---
+++ LawСRM/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawСRM.ViewModels.Base
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        //реализация события из интерйейса INotifyPropertyChanged, оповещает об изменении свойства
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //метод, вызывающий событие PropertyChanged и уведомляющий об изменени конкретного свойства через его имя
+        //параметр [CallerMemberName]string PropertyName=null можно не передавать, он будет самостоятельно определяться компилятором
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        //Метод, предназначенный для установки значения свойства
+        //Решает задачу "разрешения" кольцевого изменения свойств, когда изменение одного свойства влияет на автоизменение другого и т.д.
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    
+
+    }
+}
