dcbecb34d3e49845fa37119a7dc44204901d64fe
Irina Makedonska <iriwamaked@gmail.com>
2024-01-17 21:20:26 +0200

    Привязка VM к окну

--- LawСRM/ViewModels/MainWindowViewModel.cs
+++ LawСRM/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LawСRM.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace LawСRM.ViewModels
 {
-    public class MainWindowViewModel:ViewModels.Base.ViewModel
+    internal class MainWindowViewModel:ViewModel
     {
 
     }
