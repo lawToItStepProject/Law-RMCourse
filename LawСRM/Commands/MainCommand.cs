12d523a47a10300c85aadc887a7966d32221fa70
Irina Makedonska <iriwamaked@gmail.com>
2024-01-17 23:20:48 +0200

    Главный класс Команды

---
+++ LawСRM/Commands/MainCommand.cs
@@ -0,0 +1,25 @@
+﻿using LawСRM.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawСRM.Commands
+{
+    internal class MainCommand:Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func <object, bool> _CanExecute;
+
+        //конструктор принимает два делегата
+        public MainCommand(Action<object> Execute, Func<object, bool> CanExecute=null) 
+        { 
+            _Execute = Execute?? throw new ArgumentException(nameof(Execute);
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter)??true;
+
+        public override void Execute(object parameter)=>_Execute(parameter);
+    }
+}
