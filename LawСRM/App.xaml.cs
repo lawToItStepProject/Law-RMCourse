429d59886990b06608ceac37626d8090ef47c959
Irina Makedonska <iriwamaked@gmail.com>
2024-01-19 17:47:19 +0200

    Создание хоста и контейнера сервисов

--- LawСRM/App.xaml.cs
+++ LawСRM/App.xaml.cs
@@ -1,14 +1,41 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
 using System.Windows;
 
 namespace LawСRM
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
+        //Описываем хост, используя паттерн Singleton (первое обращение к статическому свойству заставит хост создаться
+        //с помощью метода CreateHostBuilder, а все последующие обращения будут переадресованы к полю, которое будет заполнено
+        //при первом обращении к нему
+        private static IHost _Host;
+
+        private static IHost Host=>_Host??=Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        //Создаем метод для доступа к сервисам через хост. Таким образом первое обращение к этому свойству приведет к созданию хоста
+        public static IServiceProvider Services => _Host.Services;
+        //Cоздаем метод, в котором будем конфиругировать сервисы приложения
+        internal static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 
 }
